c908a8c0c2c992a81659d0abf2c5dee31e2b31af
Nguyen Ba Tuan <tuan2k1tvtb@gmail.com>
2023-08-25 17:19:15 +0700

    17:19

--- Frontend/src/components/home/home.footer.tsx
+++ Frontend/src/components/home/home.footer.tsx
@@ -1,8 +1,30 @@
 "use client";
 import "../../app/style/style.css";
+import { Button } from "react-bootstrap";
+
+const buttonStyle = {
+    outline: 'none',
+    borderRadius: '10px',
+    display: 'flex',
+    width: '64px',
+    padding: '20px',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: '10px',
+    border: '1px solid rgba(183, 187, 203, 0.5)',
+    background: 'linear-gradient(138deg, #1d222e 0%, #293140 100%)',
+  };
+
+  const footerStyle={
+    display:'flex',
+    with: '100%',
+    justifyContent: 'center',
+  }
 const FooterHome = () =>{
     return(
-        <footer>footer</footer>
+        <footer style={footerStyle}>
+            <button style={buttonStyle}>123</button>
+        </footer>
     )
 }
 export default FooterHome;
